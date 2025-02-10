b91e47828cf54e121102dac1b78f671639818d4f
PengKejie <pengkejie@h-partners.com>
2025-02-10 17:11:52 +0800

    change char* to uint8_t*
    Signed-off-by: PengKejie <pengkejie@h-partners.com>

--- ohos/src/hyphen-build/hyphen_pattern_processor.cpp
+++ ohos/src/hyphen-build/hyphen_pattern_processor.cpp
@@ -37,9 +37,8 @@ vector<uint16_t> ConvertToUtf16(const string& utf8Str)
     uint32_t c = 0;
     vector<uint16_t> target;
     const int32_t textLength = static_cast<int32_t>(utf8Str.size());
-    const uint8_t* utf8StrPtr = reinterpret_cast<const uint8_t*>(utf8Str.c_str());
     while (i < textLength) {
-        U8_NEXT(utf8StrPtr, i, textLength, c);
+        U8_NEXT(reinterpret_cast<const uint8_t*>(utf8Str.c_str()), i, textLength, c);
         if (U16_LENGTH(c) == 1) {
             target.push_back(c);
         } else {
